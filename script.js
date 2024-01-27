2c6282c6682f41de93162791111c52d7d23f1566
Jerry bony <jbony@icstars.org>
2024-01-27 13:57:16 -0300

    date picker

---
+++ script.js
@@ -0,0 +1,92 @@
+import {
+  format,
+  fromUnixTime,
+  getUnixTime,
+  addMonths,
+  subMonths,
+  startOfWeek,
+  startOfMonth,
+  endOfWeek,
+  endOfMonth,
+  eachDayOfInterval,
+  isSameMonth,
+  isSameDay,
+  set
+} from "date-fns";
+const datePickerButton = document.querySelector(".date-picker-button");
+const datePicker = document.querySelector(".date-picker");
+const datePickerHeaderText = document.querySelector(".current-month");
+const previousMonthButton = document.querySelector(".prev-month-button");
+const nextMonthButton = document.querySelector(".next-month-button");
+const dateGrid = document.querySelector(".date-picker-grid-dates");
+
+let currentDate = new Date();
+
+datePickerButton.addEventListener("click", () => {
+  datePicker.classList.toggle("show");
+  const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+  currentDate = selectedDate;
+  setUpDatePicker(selectedDate);
+});
+
+function setDate(date) {
+  datePickerButton.innerText = format(date, "MMMM do, yyyy");
+  datePickerButton.dataset.selectedDate = getUnixTime(date);
+}
+
+
+function setUpDatePicker(currentDate) {
+  datePickerHeaderText.innerText = format(currentDate, "MMMM - yyyy");
+  setUpDates(currentDate);
+}
+
+function setUpDates (selectedDate) {
+  const firstWeekStart = startOfWeek(startOfMonth(currentDate));
+  const lastWeekEnd = endOfWeek(endOfMonth(currentDate));
+  const dates = eachDayOfInterval({start: firstWeekStart, end: lastWeekEnd});
+  dateGrid.innerHTML = "";
+  dates.forEach(date => {
+    const dateElement = document.createElement("button");
+    dateElement.classList.add("date");
+    dateElement.innerText = date.getDate();
+    if(!isSameMonth(date, currentDate)) {
+      dateElement.classList.add("date-picker-other-month-date");
+    }
+    if (isSameDay(date, selectedDate)) {
+      dateElement.classList.add("selected");
+    }
+
+    dateElement.addEventListener("click", () => {
+      setDate(date);
+      console.log(date);
+      datePicker.classList.remove("show");
+    });
+
+    dateGrid.appendChild(dateElement);
+  })
+}
+  nextMonthButton.addEventListener(
+
+    
+    "click",
+    () => {
+      const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+        currentDate = addMonths(currentDate, 1);
+        console.log(currentDate);
+      setUpDatePicker(selectedDate);
+    }
+  );
+
+  previousMonthButton.addEventListener(
+    "click",
+    () => {
+      const selectedDate = fromUnixTime(datePickerButton.dataset.selectedDate);
+      console.log("previous month button clicked");
+      currentDate = subMonths(currentDate, 1);
+      console.log(currentDate);
+      setUpDatePicker(selectedDate);
+    }
+  );
+
+
+setDate(new Date());
